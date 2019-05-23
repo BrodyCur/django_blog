2af9e6fa22c7b1031c9c8b6f3462cc755cc082d7
Brody Currie <brodycurrie@gmail.com>
2019-05-23 16:33:02 -0400

    created django blog and populated home page with content

---
+++ blog/views.py
@@ -0,0 +1,11 @@
+from django.http import HttpResponse, HttpResponseRedirect
+from django.shortcuts import render
+from datetime import date
+
+def home_page(request):
+  current_date = date.today()
+  context = {
+    'date': current_date,
+    }
+  response = render(request, 'index.html', context)
+  return HttpResponse(response)
